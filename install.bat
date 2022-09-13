416c49143aa00e187aa831a55fab0ac56b9a42de
Coderexe <76922985+1Coderexe@users.noreply.github.com>
2022-09-13 13:50:36 -0700

    Add files via upload

---
+++ install.bat
@@ -0,0 +1,5 @@
+powershell -Command "Invoke-WebRequest https://raw.githubusercontent.com/1Coderexe/DirectDownloads/main/AutoSchoolLogin.py -OutFile AutoSchoolLogin.py"
+
+powershell -Command "Invoke-WebRequest https://raw.githubusercontent.com/1Coderexe/DirectDownloads/main/password.txt -OutFile password.txt"
+
+powershell -Command "Invoke-WebRequest https://raw.githubusercontent.com/1Coderexe/DirectDownloads/main/username.txt -OutFile username.txt"
