354b98a6e613b747340b1aec605b4fca5a06dbc9
Coderexe <76922985+1Coderexe@users.noreply.github.com>
2022-09-13 13:53:41 -0700

    Update install.bat

--- install.bat
+++ install.bat
@@ -1,4 +1,4 @@
-powershell -Command "Invoke-WebRequest https://raw.githubusercontent.com/1Coderexe/DirectDownloads/main/AutoSchoolLogin.py -OutFile AutoSchoolLogin.pyw"
+powershell -Command "Invoke-WebRequest https://raw.githubusercontent.com/1Coderexe/DirectDownloads/main/AutoSchoolLogin.pyw -OutFile AutoSchoolLogin.pyw"
 
 powershell -Command "Invoke-WebRequest https://raw.githubusercontent.com/1Coderexe/DirectDownloads/main/password.txt -OutFile password.txt"
 
